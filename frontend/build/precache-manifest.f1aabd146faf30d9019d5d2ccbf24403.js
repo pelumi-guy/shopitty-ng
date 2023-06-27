8ab6d54aada3bcaebc63f7d74fac2f607930f965
pelumi <polalekan@gmail.com>
2023-06-27 01:46:48 +0100

    pushing frontend build

---
+++ frontend/build/precache-manifest.f1aabd146faf30d9019d5d2ccbf24403.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a7214fe330cb86336a1",
+    "url": "/static/css/main.a24cedd1.chunk.css"
+  },
+  {
+    "revision": "6a7214fe330cb86336a1",
+    "url": "/static/js/main.6a7214fe.chunk.js"
+  },
+  {
+    "revision": "4552a7aa00aaaec5277f",
+    "url": "/static/css/1.9c299164.chunk.css"
+  },
+  {
+    "revision": "4552a7aa00aaaec5277f",
+    "url": "/static/js/1.4552a7aa.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "31e1300d419245fd27614630601dc74d",
+    "url": "/static/media/bootstrap-icons.31e1300d.woff2"
+  },
+  {
+    "revision": "be6df3f0c9855796ae0c099ee4ca43ce",
+    "url": "/static/media/bootstrap-icons.be6df3f0.woff"
+  },
+  {
+    "revision": "e805be6cc4531d890ac912116638a48b",
+    "url": "/index.html"
+  }
+];
