ed6b356cb08e7d437d87a423457b178d376c8fd3
pelumi <polalekan@gmail.com>
2023-07-07 17:15:41 +0100

    debugging /me no response in production

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -8,6 +8,8 @@ exports.isAuthenticatedUser = catchAsyncErrors( async (req, res, next) => {
 
     const { token } = req.cookies;
 
+    console.log({token})
+
     if (!token) {
         return next(new ErrorHandler("Login first to access this resource.", 401));
     }
